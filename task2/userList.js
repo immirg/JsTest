f937c795f09d7b5b0b9d8dbfeed9b64f7f285485
IuriiBogdanov <immirg@gmail.com>
2025-11-23 19:07:38 +0200

    implementation of the second task

---
+++ task2/userList.js
@@ -0,0 +1,25 @@
+// import {userCard} from "./userDetails.js";
+const divWrapper = document.getElementById('wrapper');
+
+async function allUsers() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+
+    for (const {id, name} of users) {
+        const div = document.createElement("div");
+        const p = document.createElement("p");
+        const button = document.createElement("button");
+        const a = document.createElement("a");
+
+        a.href = `user-details.html?id=${id}`;
+        p.innerText = `id: ${id} - ${name}`;
+        button.innerText = 'details';
+        button.id = `button_${id}`;
+        div.classList.add('user-block');
+
+        a.appendChild(button);
+        div.append(p, a);
+        divWrapper.appendChild(div);
+    }
+}
+allUsers();
